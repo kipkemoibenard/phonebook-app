02e645e0aa9f10c7065060802590d22f33975cc3
benard.kipkemoi <benard.kipkemoi@turnkeyafrica.com>
2024-12-21 12:23:07 +0300

    Add navbar and default homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'phonebook/contact-list', pathMatch: 'full' },
   { path: 'phonebook', loadChildren: () => import('./features/phonebook/phonebook.module').then(m => m.PhonebookModule)},
 ];
 
